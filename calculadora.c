1dc21b21fba70db6098967e4f40716ba0d672113
Rene <rene.az91@gmail.com>
2013-04-26 21:18:20 -0400

    Version Inicial

---
+++ calculadora.c
@@ -0,0 +1,454 @@
+/*
+ *	Analizador Léxico	
+ *	Curso: Compiladores y Lenguajes de Bajo de Nivel
+ *	Práctica de Programación Nro. 1
+ *	
+ *	Descripcion:
+ *	Implementa una analizador léxico para una calculadora de expresiones
+ *  aritmeticas que reconoce números (enteros, reales, notación científica),
+ *  operadores, y comentarios de un archivo fuente con lineas de operaciones aritméticas.
+ *	
+ * 	Despliega cada linea con los lexemas -> token
+ *  y al final de la misma la expresión que representa línea
+ */
+
+/*********** Librerias utilizadas **************/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+/***************** MACROS **********************/
+
+//INPUTS
+#define FACTOR		300
+
+#define OPSUMA		256
+#define OPRESTA		257
+#define OPMULT		258
+#define OPDIV		259
+
+#define OPASIGNA	260
+
+// Fin INPUTS
+#define TAMBUFF 	5
+#define TAMLEX 		50
+#define TAMHASH 	101
+
+/************* Definiciones ********************/
+
+typedef struct entrada{
+	//definir los campos de 1 entrada de la tabla de simbolos
+	int compLex;
+	char lexema[TAMLEX];	
+	struct entrada *tipoDato; // null puede representar variable no declarada	
+	// aqui irian mas atributos para funciones y procedimientos...
+	
+} entrada;
+
+typedef struct {
+	int compLex;
+	entrada *pe;
+} token;
+
+/************* Variables globales **************/
+
+int consumir;			/* 1 indica al analizador lexico que debe devolver
+						el sgte componente lexico, 0 debe devolver el actual */
+
+char cad[5*TAMLEX];		// string utilizado para cargar mensajes de error
+token t;				// token global para recibir componentes del Analizador Lexico
+
+// variables para el analizador lexico
+
+FILE *archivo;			// Fuente con Expresiones aritméticas
+char buff[2*TAMBUFF];	// Buffer para lectura de archivo fuente
+char id[TAMLEX];		// Utilizado por el analizador lexico
+int delantero=-1;		// Utilizado por el analizador lexico
+int fin=0;				// Utilizado por el analizador lexico
+int numLinea=1;			// Numero de Linea
+int mostrar=0;
+char expresion[TAMLEX*4];
+/************** Prototipos *********************/
+
+
+void sigLex();		// Del analizador Lexico
+
+/**************** Funciones **********************/
+int stricmp(const char* cad,const char* cad2) 
+{
+	int i;
+	char c1[strlen(cad)];
+	char c2[strlen(cad2)];
+	
+	strcpy(c1,cad);
+	strcpy(c2,cad2);
+	for(i=0;i<strlen(c1);i++)
+		c1[i]=tolower(c1[i]);
+
+	for(i=0;i<strlen(c2);i++)
+		c2[i]=tolower(c2[i]);
+
+	return strcmp(c1,c2);
+}
+
+/*********************HASH************************/
+entrada *tabla;				//declarar la tabla de simbolos
+int tamTabla=TAMHASH;		//utilizado para cuando se debe hacer rehash
+int elems=0;				//utilizado para cuando se debe hacer rehash
+
+int h(const char* k, int m)
+{
+	unsigned h=0,g;
+	int i;
+	for (i=0;i<strlen(k);i++)
+	{
+		h=(h << 4) + k[i];
+		if (g=h&0xf0000000)
+		{
+			h=h^(g>>24);
+			h=h^g;
+		}
+	}
+	return h%m;
+}
+void insertar(entrada e);
+
+void initTabla()
+{	
+	int i=0;
+	
+	tabla=(entrada*)malloc(tamTabla*sizeof(entrada));
+	for(i=0;i<tamTabla;i++)
+	{
+		tabla[i].compLex=-1;
+	}
+}
+
+int esprimo(int n)
+{
+	int i;
+	for(i=3;i*i<=n;i+=2)
+		if (n%i==0)
+			return 0;
+	return 1;
+}
+
+int siguiente_primo(int n)
+{
+	if (n%2==0)
+		n++;
+	for (;!esprimo(n);n+=2);
+
+	return n;
+}
+
+//en caso de que la tabla llegue al limite, duplicar el tamaño
+void rehash()
+{
+	entrada *vieja;
+	int i;
+	vieja=tabla;
+	tamTabla=siguiente_primo(2*tamTabla);
+	initTabla();
+	for (i=0;i<tamTabla/2;i++)
+	{
+		if(vieja[i].compLex!=-1)
+			insertar(vieja[i]);
+	}		
+	free(vieja);
+}
+
+//insertar una entrada en la tabla
+void insertar(entrada e)
+{
+	int pos;
+	if (++elems>=tamTabla/2)
+		rehash();
+	pos=h(e.lexema,tamTabla);
+	while (tabla[pos].compLex!=-1)
+	{
+		pos++;
+		if (pos==tamTabla)
+			pos=0;
+	}
+	tabla[pos]=e;
+
+}
+//busca una clave en la tabla, si no existe devuelve NULL, posicion en caso contrario
+entrada* buscar(const char *clave)
+{
+	int pos;
+	entrada *e;
+	pos=h(clave,tamTabla);
+	while(tabla[pos].compLex!=-1 && stricmp(tabla[pos].lexema,clave)!=0 )
+	{
+		pos++;
+		if (pos==tamTabla)
+			pos=0;
+	}
+	return &tabla[pos];
+}
+
+void insertTablaSimbolos(const char *s, int n)
+{
+	entrada e;
+	sprintf(e.lexema,s);
+	e.compLex=n;
+	insertar(e);
+}
+
+void initTablaSimbolos()
+{
+	entrada pr,*e;
+	insertTablaSimbolos("+",OPSUMA);
+	insertTablaSimbolos("-",OPRESTA);
+	insertTablaSimbolos("*",OPMULT);
+	insertTablaSimbolos("/",OPDIV);
+	insertTablaSimbolos("=",OPASIGNA);
+}
+
+// Rutinas del analizador lexico
+
+void error(const char* mensaje)
+{
+	printf(" //Error Lexico. %s.\n",mensaje);	
+}
+
+void sigLex()
+{
+	int i=0;
+	char c=0;
+	int acepto=0;
+	int estado=0;
+	char msg[41];
+	entrada e;
+
+	while((c=fgetc(archivo))!=EOF)
+	{
+		
+		if (c==' ' || c=='\t')
+			continue;	//eliminar espacios en blanco
+		else if(c=='\n')
+		{
+			numLinea++;//incrementar el numero de linea
+			c=fgetc(archivo);
+			if (c!='\n')
+				mostrar=1;
+			ungetc(c,archivo);
+			continue;
+		}
+		else if (isdigit(c))
+		{
+				//es un numero
+				i=0;
+				estado=0;
+				acepto=0;
+				id[i]=c;
+				
+				while(!acepto)
+				{
+					switch(estado){
+					case 0: //una secuencia netamente de digitos, puede ocurrir . o e
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=0;
+						}
+						else if(c=='.'){
+							id[++i]=c;
+							estado=1;
+						}
+						else if(tolower(c)=='e'){
+							id[++i]=c;
+							estado=3;
+						}
+						else{
+							estado=6;
+						}
+						break;
+					
+					case 1://un punto, debe seguir un digito
+						c=fgetc(archivo);						
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=2;
+						}
+						else{
+							sprintf(msg,"No se esperaba '%c'",c);
+							estado=-1;
+						}
+						break;
+					case 2://la fraccion decimal, pueden seguir los digitos o e
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=2;
+						}
+						else if(tolower(c)=='e')
+						{
+							id[++i]=c;
+							estado=3;
+						}
+						else
+							estado=6;
+						break;
+					case 3://una e, debe proseguir un dígito o una secuencia de digitos
+						c=fgetc(archivo);
+						if (c=='+' || c=='-')
+						{
+							id[++i]=c;
+							estado=4;
+						}
+						else if(isdigit(c))
+						{
+							id[++i]=c;
+							estado=5;
+						}
+						else{
+							sprintf(msg,"No se esperaba '%c'",c);
+							estado=-1;
+						}
+						break;
+					case 4://necesariamente debe venir por lo menos un digito
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=5;
+						}
+						else{
+							sprintf(msg,"No se esperaba '%c'",c);
+							estado=-1;
+						}
+						break;
+					case 5://una secuencia de digitos
+						c=fgetc(archivo);
+						if (isdigit(c))
+						{
+							id[++i]=c;
+							estado=5;
+						}
+						else{
+							estado=6;
+						}break;
+					case 6:	//estado de aceptacion, 
+							//devolver el caracter correspondiente a otro componente lexico
+						if (c!=EOF)
+							ungetc(c,archivo);
+						else
+							c=0;
+						id[++i]='\0';
+						acepto=1;
+						t.pe=buscar(id);
+						if (t.pe->compLex==-1)
+						{
+							sprintf(e.lexema,id);
+							e.compLex=FACTOR;
+							insertar(e);
+							t.pe=buscar(id);
+						}
+						t.compLex=FACTOR;
+						break;
+					case -1:
+						if (c==EOF)
+							error("No se esperaba el fin de archivo");
+						else
+							error(msg);
+						exit(1);
+					}
+				}
+			break;
+		}
+		else if (c=='+')
+		{
+			t.compLex=OPSUMA;
+			t.pe=buscar("+");
+			break;
+		}
+		else if (c=='-')
+		{
+			t.compLex=OPRESTA;
+			t.pe=buscar("-");
+			break;
+		}
+		else if (c=='*')
+		{
+			t.compLex=OPMULT;
+			t.pe=buscar("*");
+			break;
+		}
+		else if (c=='/')
+		{
+			if ((c=fgetc(archivo))=='/')
+            {//la secuencia es un comentario
+                while(c!=EOF && c!='\n')
+                {
+                    c=fgetc(archivo);					
+                }
+                ungetc(c,archivo);
+            }
+            else
+            {
+				ungetc(c,archivo);
+                t.compLex=OPDIV;
+                t.pe=buscar("/");
+                break;
+            }
+		}
+		else if (c=='=')
+		{
+			t.compLex=OPASIGNA;
+			t.pe=buscar("=");
+			break;
+		}
+	}
+	if (c==EOF)
+	{
+		t.compLex=EOF;
+		sprintf(e.lexema,"EOF");
+		t.pe=&e;
+	}
+	
+}
+
+int main(int argc,char* args[])
+{
+	// inicializar analizador lexico
+	int complex=0;
+	initTabla();
+	initTablaSimbolos();
+	
+	if(argc > 1)
+	{
+		if (!(archivo=fopen(args[1],"rt")))
+		{
+			printf("Archivo no encontrado.\n");
+			exit(1);
+		}
+		while (t.compLex!=EOF){
+			sigLex();
+			if (mostrar==1){
+				printf("**********************************\n");
+				printf("EXPRESION: %s \n",expresion);
+				printf("**********************************\n");
+				memset(&expresion[0], 0, sizeof(expresion));
+				mostrar=0;
+			}
+			else 
+				printf("Lin %d: %s -> %d\n",numLinea,t.pe->lexema,t.compLex);
+			strcat(expresion, t.pe->lexema);
+			
+		}
+		fclose(archivo);
+	}else{
+		printf("Debe pasar como parametro el path al archivo fuente.\n");
+		exit(1);
+	}
+
+	return 0;
+}
